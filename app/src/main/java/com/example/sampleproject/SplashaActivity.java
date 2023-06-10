64071a682f2a7f0440ccf697ea03358fe31e534a
jobaer-hassan <jobaerhassan3@gmail.com>
2023-06-10 09:24:54 +0600

    first

---
+++ app/src/main/java/com/example/sampleproject/SplashaActivity.java
@@ -0,0 +1,29 @@
+package com.example.sampleproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splasha);
+
+        if(getSupportActionBar() != null)
+        {
+            getSupportActionBar().hide();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent toMainActivity = new Intent(SplashaActivity.this, LoginActivity.class);
+                startActivity(toMainActivity);
+            }
+        },3000);//3 sec delay
+    }
+}
